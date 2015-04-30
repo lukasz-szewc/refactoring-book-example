314632816aa5ddb67ea94915c43e34efe5e17b99
Lukasz Szewc <lukasz-szewc@users.noreply.github.com>
2015-04-30 19:56:08 +0200

    performed move, method amountFor moved from Customer class into Rental class (feature envy?)

--- src/main/java/pl/refactoring/Customer.java
+++ src/main/java/pl/refactoring/Customer.java
@@ -29,7 +29,7 @@ public class Customer {
 
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            double thisAmount = amountFor(each);
+            double thisAmount = each.amountFor();
 
             frequentRenterPoints++;
 
@@ -47,26 +47,6 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points\n";
 
         return result;
-    }
-
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return thisAmount;
     }
 
 }
